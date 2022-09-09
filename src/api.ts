06d8ad8ed8b8a1939c4bb8eeda26a96dfa4b6df0
talhasch <talhabugrabulut@gmail.com>
2022-09-09 17:39:07 +0300

    adds getAccountBalances

--- src/api.ts
+++ src/api.ts
@@ -41,3 +41,33 @@ export const getAccountBalance = (network: StacksNetwork, address: string, block
     const u = `${network.coreApiUrl}/v2/accounts/${address}?proof=0&tip=${blockTip}`;
     return axios.get(u, baseConfig).then(r => cvToValue(hexToCV(r.data.balance)));
 }
+
+interface AccountBalances {
+    stx: {
+        balance: string,
+        total_sent: string,
+        total_received: string,
+        total_fees_sent: string,
+        total_miner_rewards_received: string,
+        lock_tx_id: string,
+        locked: string,
+        lock_height: number,
+        burnchain_lock_height: number,
+        burnchain_unlock_height: number
+    },
+    fungible_tokens: Record<string, {
+        "balance": string,
+        "total_sent": string,
+        "total_received": string
+    }>,
+    non_fungible_tokens: Record<string, {
+        "count": string,
+        "total_sent": string,
+        "total_received": string
+    }>,
+}
+
+export const getAccountBalances = (network: StacksNetwork, address: string, untilBlock: number): Promise<AccountBalances> => {
+    const u = `${network.coreApiUrl}/extended/v1/address/${address}/balances?until_block=${untilBlock}`;
+    return axios.get(u, baseConfig).then(r => cvToValue(hexToCV(r.data.balance)));
+}
